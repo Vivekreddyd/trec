c528d5f341f9da74b5b5f5fcbef3134e1abba70b
Vivekreddyd <vivek.doudagiri@gmail.com>
2016-12-27 07:22:40 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,146 @@
-# trec
+# TREC 2011 Twitter Collection Downloader
+
+* Downloads approximately 16 million tweets which are gathered by TREC in 2011. 
+* Enables download from multiple machine and save your time (in a single machine, it may last +30 days!)
+* Retry failed downloads
+* Write downloads into log file
+* Combines all failed/succeed download statistics
+* Uncompress all zips into one folder
+
+For more information about collection https://github.com/lintool/twitter-tools/wiki/Tweets2011-Collection and TREC 
+http://trec.nist.gov/data/tweets/
+
+## Prerequests
+
+* Python 3.5.*
+* Download https://github.com/lintool/twitter-tools to your local. This is main tool to fetch the tweets. Installing steps of the tool is as decribed in its page:
+
+
+  * The main Maven artifact for the TREC Microblog API is `twitter-tools-core`. The latest releases of Maven artifacts are available at [Maven Central](http://search.maven.org/#search%7Cga%7C1%7Ccc.twittertools).
+
+  * You can clone the repo with the following command:
+
+    ```
+    $ git clone git://github.com/lintool/twitter-tools.git
+    ``` 
+
+  * Once you've cloned the repository, change directory into `twitter-tools-core` and build the package with Maven:
+
+    ```
+    $ cd twitter-tools-core
+    $ mvn clean package appassembler:assemble
+    ```
+
+  * For more information, see the [project wiki](https://github.com/lintool/twitter-tools/wiki).
+
+* Than clone this repo with the following command into the `twitter-tools-core` directory that you have cd into in previous step:
+
+  ```
+  https://github.com/cmlonder/trec-collection-downloader.git
+  ```
+
+* Install required libraries by running this command:
+
+  ```
+  pip3.5 install -r requirements.txt
+  ```
+
+Or if your default python version is 3.5 than you can run this command with using directly `pip` command instead of `pip3.5`
+
+  ```
+  pip install -r requirements.txt
+  ```
+
+* If you haven't pip installed (which is must be installed default when you install Python 3.5) than follow those steps:
+
+  * To install pip, securely download [get-pip.py](https://bootstrap.pypa.io/get-pip.py)
+
+  * Then run the following:
+
+    ```
+    python3.5 get-pip.py
+    ```
+
+  * or if your default python version is 3.5 you can run this command with using directly `python` instead of `python3.5`
+
+    ```
+    python get-pip.py
+    ```
+
+  * For the rest of the tutorial, it is assumed that your default python version is 3.5 and `python` command will be used in examples.
+
+* Get your URL, username and password via http://trec.nist.gov/data/tweets/ .Pay attention to this section in the link:
+
+> Email the signed agreement, as a PDF file, to Angela Ellis <angela.ellis@nist.gov>. In the body of your email,
+> Be clear that you are requesting the Tweets2011 dataset
+> Include your name,
+> your email address, and
+> the name of your organization.
+> We will respond to your request with a URL, a username, and a password with which you can download the tweet lists. Please allow seven business days for a respons
+
+
+## Folder Structure
+
+Here is the folder structure which this script will create.
+
+**tarFiles:** Compressed files which is firstly downloaded from the site you have received from the TREC authorities.
+
+**openedTarFiles:** Uncompressed files which are structured like DAY/DAY-NUMBER.dat. There are 17 days and 100 dat files in each day
+
+**jsonFiles:** Compressed JSON files that fetched by using dat files. There are approximately 10.000 tweets in each file
+
+**repairJsonFiles:** JSON files which has the information that failed fetches. Those will be downloaded automatically by the script into jsonFiles folder with the extension *.repair.json.gz*
+
+**logFiles:** Log files for each json file.
+
+
+## Usage
+
+1) Update url, user and password with your own ones
+
+```python
+url = 'YOUR_URL_HERE'
+user, password = 'YOUR_USER_NAME_HERE', 'YOUR_PASSWORD_HERE'
+```
+
+2) Run the script for default usage. This will fetch tweets(10.000 per .dat file) from each .dat file (100 per folder) from each folder (17 folder). This is not good scenario cause it will cost you +30 more days, but if you have acceptable time and connection go with this one.
+
+```
+python trec.py
+```
+
+3) Download the corpus with reverse order.
+
+```
+python trec.py --reverse
+```
+
+4) Download the corpus from folder number 5 to 10
+
+```
+python trec.py 5 10
+```
+
+5) Download the corpus from folder number 0 to 5 with reverse order
+
+```
+python trec.py --inverse 0 5
+```
+
+6) Download the corpus from 0 (default beginning number if only 1 number is given) to 5
+
+```
+python trec.py 5
+```
+
+## Example Scenario
+
+You have 4 machine to download the corpus. Or you can start 4 different command line in a single machine if you have good bandwith and processor. It will be faster than single connection. So run the script in each machine with given ranges and than merge your jsonFiles folders. Use any number of machine like this scenario to download corpus with minimum amount of time.
+
+```
+python trec.py 0 3
+python trec.py 4 7
+python trec.py 8 11
+python trec.py 12 17
+```
+
---
+++ trec.py
@@ -0,0 +1,234 @@
+import requests
+from bs4 import BeautifulSoup
+import shutil
+import os
+import tarfile
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import gzip
+import subprocess
+import logging
+import sys
+
+# define nist website url, username and password  that you have receied here
+url = 'https://ir.nist.gov/tweets2011'
+user, password = 'tweets2011', 'NHwv6H4o'
+
+# path config, you can rename those if you wish
+currentFolder = os.path.dirname(os.path.realpath('__file__'))   
+tarFolder = 'tarFiles'
+untarFolder = 'openedTarFiles'
+jsonFolder = 'jsonFiles'
+logFolder = 'logFiles'
+repairJsonFolder = 'repairJsonFiles'
+tweetsFolder = 'tweets'
+max = 17
+
+# We wont use SSL since we trust the downloading page, so hide warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# get tweets from the dat files which are opened from the tar files
+def getTweets(untarFolder, begin, end):
+
+    list = os.listdir(untarFolder)
+
+    # skip files that may be automatically added to folder like ".DS_STORE" in OS X
+    for file in list:
+        if "." in file:
+            list.remove(file)
+
+    list.sort()
+    if reverse == True:
+        list.reverse()
+
+    if end > max:
+        end = max
+        print("Setting end point to maximum")
+    
+
+    print(end-begin+1)
+    for i in range(0, end-begin):
+
+        # dat file directory is in the same level with this script file, get its path
+        datFolderPath = os.path.join(untarFolder, list[i])
+    
+        datFileList = os.listdir(datFolderPath)
+        datFileList.sort()
+        print(datFileList)
+        for datFile in datFileList:
+            # remove dat extension
+            datFileName = os.path.splitext(datFile)[0]
+
+            # Crate a path for downloaded tweets data and its repair data
+            jsonPath = makePath(jsonFolder, datFileName + ".json.gz")
+            repairJsonPath = makePath(repairJsonFolder, datFileName + ".json.gz")
+            datFilePath = makePath(datFolderPath, datFile)
+            fetcherPath = os.path.join("..", "target", "appassembler", "bin", "AsyncHTMLStatusBlockCrawler")
+      
+            with subprocess.Popen(['sh', fetcherPath, '\\'
+   ,'-data',datFilePath,'-output', jsonPath, '\\', '-repair', repairJsonPath], stdout=subprocess.PIPE) as proc:
+                path = makePath(logFolder,datFileName+".log")
+                print("logging to:" + path)
+                logging.basicConfig(filename=path, level=logging.DEBUG)
+                logging.info(proc.stdout.read().decode('utf-8'))
+    return
+
+# Repair failed tweets
+def repairTweets(repairJsonFolder):
+    for repairFile in os.listdir(repairJsonFolder):
+        repairFilePath = os.path.join(repairJsonFolder, repairFile)
+        repairFile = os.path.splitext(repairFile)[0]
+        fetcherPath = os.path.join("..", "target", "appassembler", "bin", "AsyncHTMLStatusBlockCrawler")
+
+        if os.path.isdir(repairFilePath) == True:
+            repairTweets(repairFilePath)
+        else:
+            repairFileName = makePath(jsonFolder, repairFile + ".repair.json.gz")
+            with subprocess.Popen(['sh', fetcherPath ,'\\'
+   ,'-data', repairFilePath, '-output', repairFileName], stdout=subprocess.PIPE) as proc:
+                path = makePath(logFolder, repairFile+".repair.log")
+                print("logging repair to:" + path)
+                logging.basicConfig(filename=path, level=logging.DEBUG)
+                logging.info(proc.stdout.read().decode('utf-8'))
+    return
+
+# Open gz file by creating new folder in  current directory
+def openGzFiles(files):
+    datFolders = []
+    for file in files:
+        tarFolder = os.path.abspath(os.path.join(file, "..","..", untarFolder))
+
+        if ".tar" in file:
+            with tarfile.open(file) as tar:
+                tar.extractall(path=tarFolder)
+                print("opening .tar.gz " + file)
+                datFolders.append(tarFolder)
+        else:
+            baseName = os.path.basename(file)
+            path = makePath(tarFolder, baseName)
+            fileName = os.path.splitext(path)[0] 
+            with gzip.open(file, 'rb') as file_in, open(fileName, 'wb') as file_out:
+                file_content =file_in.read()
+                file_out.write(file_content)
+                print("opening .gz " + path)
+                datFolders.append(path)
+    return
+
+def openTweets():
+    os.makedirs(os.path.join(currentFolder,tweetsFolder), exist_ok=True)
+    dirName = os.path.join(currentFolder, jsonFolder)
+    for file in os.listdir(dirName):
+        path = makePath(tweetsFolder, file)
+        file = os.path.join(currentFolder, jsonFolder, file)
+        fileName = os.path.splitext(path)[0]
+        with gzip.open(file, 'rb') as file_in, open(fileName, 'wb') as file_out:
+            file_content = file_in.read()
+            file_out.write(file_content)
+            print("opening json.gz" + path)                
+
+# Download retrieved files from the webpage
+def getFiles(files, begin, end):
+    pathlist = []
+    if reverse == True:
+        files.reverse()
+
+    if end > max:
+        end = max
+        print("Setting end point to maximum")
+
+    for i in range (begin, end):
+        print(str(i) + " " + str(begin) + " " + str(end))
+        os.makedirs(os.path.join(currentFolder,tarFolder), exist_ok=True)
+        downloadedFiles = os.listdir(tarFolder)
+        # if files are already downloaded skip those
+        if files[i] in downloadedFiles:
+            path = makePath(tarFolder, str(files[i]))
+            print(str(files[i]) + " is found in the path and will not be downloaded")
+            pathlist.append(path)
+            continue
+
+        downloadURL = os.path.join(url, str(files[i]))
+        resp = requests.get(downloadURL, auth=(user, password), verify=False, stream=True)
+        if (resp.status_code == 200):
+            print(downloadURL + ' is being downloaded.')
+            path = makePath(tarFolder, str(files[i]))
+            with open(path, 'wb') as file:
+                resp.raw.decode_content = True
+                shutil.copyfileobj(resp.raw, file)
+            pathlist.append(path)
+        else:
+            print("Error while downloading " + downloadURL + " code : " + str(resp.status_code))
+    return pathlist
+
+# Create new folder inside the scripts path and create new files in this new path
+
+def makePath(folderName, fileName): 
+    # use os module to join those beacuse of different operating sytems have different file seperators
+    file_path = os.path.join(currentFolder,folderName, fileName)
+
+    # Create new dir if not exist
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    return file_path
+
+
+
+# Download the tar file URLs from the parsed content
+def getFileLinks(resp):
+    fileList = []
+    html = BeautifulSoup(resp.content, 'html.parser')
+    print('Links are being added to download list.')
+    for link in html.find_all('a'):
+        if 'tar' in link.get('href'):
+            fileList.append(link.get('href'))
+    return fileList
+
+# Connect to the webpage and authenticate
+def connect(url, usr, pwd):
+    resp = requests.get(url, auth=(user, password), verify=False)
+    if(resp.status_code == 200): 
+        print('Connection successful:' + str(resp.status_code))
+        return resp
+    else:
+        print ('There is an error while connecting the site : ' + str(resp.status_code))
+
+
+# cmd config
+reverse = False
+numberOfFolders = None
+
+temp = 0
+begin = 0
+end = sys.maxsize
+
+for arg in sys.argv:
+    if arg != os.path.basename(__file__):
+        if arg == "--reverse":
+            reverse = True
+        else:
+            try:
+                number = int(arg)
+            except ValueError:
+                print("Your input must be an integer for begin and end range")
+            else:
+                # sort the numbers in increasing order, if user input is 5 2, set begin to 2 and end to 5
+                if(number >= 0):
+                    if(number > temp):
+                        begin = temp
+                        end = number
+                        temp = end
+                    else:
+                        begin = number
+                        end = temp
+                        temp = end
+                else:
+                    print("Please enter a positive integer")
+
+# let the fun begin
+
+resp = connect(url, user, password)
+tar = getFileLinks(resp)
+downloadedTar = getFiles(tar, begin, end)
+openGzFiles(downloadedTar)
+getTweets(untarFolder, begin, end)
+repairTweets(repairJsonFolder)
+#openTweets()
+#openRepairTweets()
